f9e6c3c5e13910161ce00994218202b33cbe2f53
Lukasz <freshbenypl@gmail.com>
2022-01-16 16:04:49 +0100

    works

--- client/src/pages/Addpost.js
+++ client/src/pages/Addpost.js
@@ -1,8 +1,25 @@
 //rfce react function components export
+import { Form, Col, Row, Input } from "antd";
 import React from "react";
 import DefaultLayout from "../components/DefaultLayout";
+const { TextArea } = Input;
 function Addpost() {
-  return <DefaultLayout>Add</DefaultLayout>;
+  return (
+    <DefaultLayout>
+      <Row>
+        <Col lg={12}>
+          <Form>
+            <Form.Item name="description" label="Description">
+              <TextArea></TextArea>
+            </Form.Item>
+            <Form.Item name="image" label="Image">
+              <TextArea></TextArea>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
+    </DefaultLayout>
+  );
 }
 
 export default Addpost;
