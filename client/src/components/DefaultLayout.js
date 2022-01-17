acfd18a2e66f052eb64f339e38df62d71c633812
Lukasz <freshbenypl@gmail.com>
2022-01-17 01:07:28 +0100

    dziala

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -9,6 +9,11 @@ import {
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import "./defaultlayout.css";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Dropdown from "react-bootstrap/Dropdown";
 const { Header, Sider, Content } = Layout;
 
 class DefaultLayout extends React.Component {
@@ -27,7 +32,47 @@ class DefaultLayout extends React.Component {
       <Layout>
         <Layout className="site-layout">
           <Header className="site-layout-background " style={{ padding: 0 }}>
-            {/* bootstrap classes (equal division)*/}
+            <Navbar bg="dark" variant="dark">
+              <Nav className="container-fluid">
+                <Nav.Item>
+                  <Navbar.Brand as={Link} to="/">
+                    Lukasz Baran App
+                  </Navbar.Brand>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link as={Link} to="/user-list">
+                    Profil
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link>LogOut</Nav.Link>
+                </Nav.Item>
+                <Nav.Item className="ml-auto">
+                  <Dropdown>
+                    <Dropdown.Toggle
+                      id="dropdown-button-dark-example1"
+                      variant="secondary"
+                    >
+                      Dropdown Button
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu variant="dark">
+                      <Dropdown.Item href="/">
+                        {/* active atribute */}
+                        Home
+                      </Dropdown.Item>
+                      <Dropdown.Item href="/addpost">Addpost</Dropdown.Item>
+                      {/* <Dropdown.Divider />
+                      <Dropdown.Item href="#/action-4">
+                        Separated link
+                      </Dropdown.Item> */}
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Nav.Item>
+              </Nav>
+            </Navbar>
+
+            {/* bootstrap classes (equal division)
             <div className="d-flex justify-content-between align-items-center bs2">
               <h4 className="ml-2">
                 {JSON.parse(localStorage.getItem("user")).username}
@@ -39,11 +84,11 @@ class DefaultLayout extends React.Component {
                   className: "trigger",
                   onClick: this.toggle,
                 }
-              )}
-            </div>
+              )} 
+            </div>*/}
           </Header>
           <Content
-            className="site-layout-background"
+            className="site-layout-background mt-4"
             style={
               {
                 // margin: "24px 16px",
@@ -56,15 +101,16 @@ class DefaultLayout extends React.Component {
             {this.props.children}
           </Content>
         </Layout>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+
+        {/* <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
           <div className="logo" />
-          {/* window.location.pathname - change the active tab */}
+          window.location.pathname - change the active tab
           <Menu
             theme="dark"
             mode="inline"
             defaultSelectedKeys={[window.location.pathname]}
           >
-            {/* window.location.pathname - for the "key" */}
+            window.location.pathname - for the "key"
             <Menu.Item key="/" icon={<UserOutlined />}>
               <Link to="/"> Home</Link>
             </Menu.Item>
@@ -75,7 +121,7 @@ class DefaultLayout extends React.Component {
               <Link to="/profile"> Profile</Link>
             </Menu.Item>
           </Menu>
-        </Sider>
+        </Sider> */}
       </Layout>
     );
   }
