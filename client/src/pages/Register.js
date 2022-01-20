9c14fa7ff1c6614d5712065108d37d765da1e9a1
Lukasz <freshbenypl@gmail.com>
2022-01-20 04:15:37 +0100

    fix more bugs

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -14,68 +14,66 @@ function Register() {
   return (
     <div>
       <div className="register-login ">
-        <div className="d-flex align-items-center justify-content-center">
-          <img
-            className="mb-5  logo2"
-            src={`${process.env.PUBLIC_URL}/assets/images/Logo.png`}
-          />
-        </div>
+        <div className="card-main">
+          <div className="d-flex align-items-center justify-content-center">
+            <img
+              className=" logo2"
+              src={`${process.env.PUBLIC_URL}/assets/images/Logo.png`}
+            />
+          </div>
 
-        <Row justify="center" className="">
-          <Col lg={6} xs={15}>
-            <Form
-              layout="vertical"
-              className="bs1 p-4 align-items-center"
-              onFinish={register}
+          <Form
+            layout="vertical"
+            className="bs1 p-4 align-items-center"
+            onFinish={register}
+          >
+            <h3>Register</h3>
+            <hr></hr>
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[{ required: true }]}
             >
-              <h3>Register</h3>
-              <hr></hr>
-              <Form.Item
-                label="Username"
-                name="username"
-                rules={[{ required: true }]}
-              >
-                <Input />
-              </Form.Item>
-              <Form.Item
-                label="Password"
-                name="password"
-                rules={[{ required: true }]}
-              >
-                <Input.Password />
-              </Form.Item>
-              <Form.Item
-                label="Confirm Password"
-                name="Confirm Password"
-                type="cpassword"
-                rules={[
-                  { required: true },
-                  ({ getFieldValue }) => ({
-                    validator(_, value) {
-                      if (!value || getFieldValue("password") === value) {
-                        return Promise.resolve();
-                      }
-                      return Promise.reject(
-                        new Error(
-                          "The two passwords that you entered do not match!"
-                        )
-                      );
-                    },
-                  }),
-                ]}
-              >
-                <Input.Password />
-              </Form.Item>
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[{ required: true }]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              label="Confirm Password"
+              name="Confirm Password"
+              type="cpassword"
+              rules={[
+                { required: true },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error(
+                        "The two passwords that you entered do not match!"
+                      )
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-              <div>
-                <Button htmlType="submit">Register</Button>
-              </div>
-              <div className="pt-3">
-                <Link to="/login">Already registered, click here to login</Link>
-              </div>
-            </Form>
-          </Col>
-        </Row>
+            <div>
+              <Button htmlType="submit">Register</Button>
+            </div>
+            <div className="pt-3">
+              <Link to="/login">Already registered, click here to login</Link>
+            </div>
+          </Form>
+        </div>
       </div>
       <span className="mt-2 mb-1"></span>
     </div>
